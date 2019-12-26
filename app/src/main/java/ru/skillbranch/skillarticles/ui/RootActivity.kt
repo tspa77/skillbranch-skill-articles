5d5696e315e1134ea760540b6418b8fb914ca14b
Pavel (Work) <tspa77@gmail.com>
2019-12-26 17:27:24 +0700

    0:49:20

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -1,9 +1,14 @@
 package ru.skillbranch.skillarticles.ui
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_root.*
+import kotlinx.android.synthetic.main.layout_bottombar.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
 
 class RootActivity : AppCompatActivity() {
 
@@ -11,10 +16,27 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
         setupToolbar()
+
+        btn_like.setOnClickListener {
+            Snackbar.make(coordinator_container, "test", Snackbar.LENGTH_LONG)
+                .setAnchorView(bottombar)
+                .show()
+        }
     }
 
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // проверяем что тулбаер есть и в нём есть лого и настраиваем его
+        val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2)
+                as ImageView else null
+        logo?.scaleType = ImageView.ScaleType.CENTER_CROP
+        val lp = logo?.layoutParams as? Toolbar.LayoutParams
+        lp?.let {
+            it.width = this.dpToIntPx(40)
+            it.height = this.dpToIntPx(40)
+            it.marginEnd = this.dpToIntPx(16)
+            logo.layoutParams = it
+        }
     }
 }
---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/AricleState.kt
@@ -0,0 +1,25 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+data class ArticleState(
+    val isAuth: Boolean = false, //пользователь авторизован
+    val isLoadingContent: Boolean = true, //контент загружается
+    val isLoadingReviews: Boolean = true, //отзывы загружаются
+    val isLike: Boolean = false, //отмечено как Like
+    val isBookmark: Boolean = false, //в закладках
+    val isShowMenu: Boolean = false, //отображается меню
+    val isBigText: Boolean = false, //шрифт увеличен
+    val isDarkMode: Boolean = false, //темный режим
+    val isSearch: Boolean = false, //режим поиска
+    val searchQuery: String? = null, // поисковы запрос
+    val searchResults: List<Pair<Int, Int>> = emptyList(), //результаты поиска (стартовая и конечная позиции)
+    val searchPosition: Int = 0, //текущая позиция найденного результата
+    val shareLink: String? = null, //ссылка Share
+    val title: String? = null, //заголовок статьи
+    val category: String? = null, //категория
+    val categoryIcon: Any? = null, //иконка категории
+    val date: String? = null, //дата публикации
+    val author: Any? = null, //автор статьи
+    val poster: String? = null, //обложка статьи
+    val content: List<Any> = emptyList(), //контент
+    val reviews: List<Any> = emptyList() //комментарии
+)
