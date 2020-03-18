94cddc920f8b5255f956c14c13577712161a2412
Pavel (Work) <tspa77@gmail.com>
2020-03-18 11:22:30 +0700

    wip - переделываю по разбору с 4-го урока. 0:35:15

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
@@ -1,21 +1,103 @@
 package ru.skillbranch.skillarticles.ui.custom
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewAnimationUtils
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import com.google.android.material.shape.MaterialShapeDrawable
+import kotlinx.android.synthetic.main.layout_bottombar.view.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.ui.custom.behaviors.BottombarBehavior
+import kotlin.math.hypot
 
 class Bottombar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior {
+    var isSearchMode = false
+
+    override fun getBehavior(): CoordinatorLayout.Behavior<Bottombar> {
+        return BottombarBehavior()
+    }
+
     init {
         View.inflate(context, R.layout.layout_bottombar, this)
         val materialBg = MaterialShapeDrawable.createWithElevationOverlay(context)
         materialBg.elevation = elevation
         background = materialBg
+    }
+
+    // save state
+    override fun onSaveInstanceState(): Parcelable? {
+        val savedState = SavedState(super.onSaveInstanceState())
+        savedState.ssIsSearchMode = isSearchMode
+        return savedState
+    }
+
+    // restore state
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        super.onRestoreInstanceState(state)
+        if (state is SavedState) {
+            isSearchMode = state.ssIsSearchMode
+            reveal.isVisible = !isSearchMode
+            group_bottom.isVisible = !isSearchMode
+        }
+    }
+
+    fun setSearchState(search: Boolean) {
+        if (isSearchMode == search || !isAttachedToWindow) return
+        isSearchMode = search
+        if (isSearchMode) animateShowSearchPanel()
+        else animateHideSearchPanel()
+    }
+
+    private fun animateHideSearchPanel() {
+        group_bottom.isVisible = false
+        val endRadius = hypot(width.toFloat(), height / 2f)
+        val va = ViewAnimationUtils.createCircularReveal(
+            reveal, width, height / 2, endRadius, 0f
+        )
+        va.doOnEnd { reveal.isVisible = false }
+        va.start()
+    }
+
+    private fun animateShowSearchPanel() {
+        reveal.isVisible = false
+        val endRadius = hypot(width.toFloat(), height / 2f)
+        val va = ViewAnimationUtils.createCircularReveal(
+            reveal, width, height / 2, 0f, endRadius
+        )
+        va.doOnEnd { group_bottom.isVisible = false }
+        va.start()
+    }
+
+
+    private class SavedState : BaseSavedState, Parcelable {
+        var ssIsSearchMode: Boolean = false
+
+        constructor(superState: Parcelable?) : super(superState)
+
+        constructor(src: Parcel) : super(src) {
+            ssIsSearchMode = src.readInt() == 1
+        }
+
+        override fun writeToParcel(dst: Parcel, flags: Int) {
+            super.writeToParcel(dst, flags)
+            dst.writeInt(if (ssIsSearchMode) 1 else 0)
+        }
+
+        override fun describeContents() = 0
+
+        companion object CREATOR : Parcelable.Creator<SavedState> {
+            override fun createFromParcel(parcel: Parcel) = SavedState(parcel)
+            override fun newArray(size: Int): Array<SavedState?> = arrayOfNulls(size)
+        }
     }
 }
