944074672986b6ff8d34516abf290d41bea3356c
Pavel (Home) <tspa77@gmail.com>
2020-03-21 18:55:05 +0700

    Сделал 2-е задание - setMarginOptionally

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -1,8 +1,20 @@
 package ru.skillbranch.skillarticles.extensions
 
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
 
 
-fun View.setMarginOptionally(bottom: Int) {
-
+fun View.setMarginOptionally(
+    left: Int = marginLeft,
+    top: Int = marginTop,
+    right: Int = marginRight,
+    bottom: Int = marginBottom
+) {
+    val params = layoutParams as ViewGroup.MarginLayoutParams
+    params.setMargins(left, top, right, bottom)
+    this.requestLayout()
 }
