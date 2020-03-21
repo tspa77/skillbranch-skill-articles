ac4a8ef3377391b58c71107b636aeaa06f54b0d8
Pavel (Home) <tspa77@gmail.com>
2020-03-21 16:43:50 +0700

    Сделал 1-е задание - indexesOf

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,5 +1,11 @@
 package ru.skillbranch.skillarticles.extensions
 
-fun String.indexesOf(query: String) {
-
-}
+fun String?.indexesOf(substr: String, ignoreCase: Boolean = true) =
+    when {
+        this == null || substr.isEmpty() -> listOf()
+        else -> substr
+            .run { if (ignoreCase) toRegex(RegexOption.IGNORE_CASE) else toRegex() }
+            .findAll(this)
+            .map { it.range.first }
+            .toList()
+    }
