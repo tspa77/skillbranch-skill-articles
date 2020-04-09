b1b2826875431e16cd7dad2e33d2be5a02a50088
Pavel (Home) <tspa77@gmail.com>
2020-04-09 21:08:50 +0700

    parsing - 1:04:31

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -38,6 +38,111 @@ object MarkdownParser {
         val matcher = elementsPattern.matcher(string)
         var lastStartIndex = 0
 
+        loop@ while (matcher.find(lastStartIndex)) {
+            val startIndex = matcher.start()
+            val endIndex = matcher.end()
+
+            // if something is found then everything before - TEXT
+            if (lastStartIndex < startIndex) {
+                parents.add((Element.Text(string.subSequence(lastStartIndex, startIndex))))
+            }
+
+            // found text
+            var text: CharSequence
+
+            // groups range for iterate by groups
+            val groups = 1..1
+            var group = -1
+            // цикл чтобы итереироваться по группам
+            for (gr in groups) {
+                if (matcher.group(gr) != null) {
+                    group = gr
+                    break
+                }
+            }
+
+            when (group) {
+                // NOT FOUND -> BREAK
+                -1 -> break@loop
+
+                //UNORDERED LIST
+                1 -> {
+                    //text without "*. "
+                    text = string.subSequence(startIndex.plus(2), endIndex)
+
+                    // find inner elements
+                    val subs = findElements(text)
+                    val element = Element.UnorderedListItem(text, subs)
+                    parents.add(element)
+
+                    // next find start from position "endIndex" (last regex character)
+                    lastStartIndex = endIndex
+                    // 01:03:50 - объяснение
+
+                }
+
+                //HEADER
+                2 -> {
+                    //text without "{#} "
+                    //TODO implement me
+                }
+
+                //QUOTE
+                3 -> {
+                    //text without "> "
+                    //TODO implement me
+                }
+
+                //ITALIC
+                4 -> {
+                    //text without "*{}*"
+                    //TODO implement me
+                }
+
+                //BOLD
+                5 -> {
+                    //text without "**{}**"
+                    //TODO implement me
+                }
+
+                //STRIKE
+                6 -> {
+                    //text without "~~{}~~"
+                    //TODO implement me
+                }
+
+                //RULE
+                7 -> {
+                    //text without "***" insert empty character
+                    //TODO implement me
+                }
+
+                //RULE
+                8 -> {
+                    //text without "`{}`"
+                    //TODO implement me
+                }
+
+                //LINK
+                9 -> {
+                    //full text for regex
+                    //TODO implement me
+                }
+                //10 -> BLOCK CODE - optionally
+                10 -> {
+                    //TODO implement me
+                }
+
+                //11 -> NUMERIC LIST
+                11 -> {
+                    //TODO implement me
+                }
+
+
+            }
+
+        }
+
         return parents
     }
 }
