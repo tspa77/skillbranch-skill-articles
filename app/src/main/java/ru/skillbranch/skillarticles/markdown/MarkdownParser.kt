b8f3a4c0fc424ac69955c4430363fe4ee8195de6
Pavel (Home) <tspa77@gmail.com>
2020-04-10 15:57:46 +0700

    parsing - 1:13:05 QUOTE_GROUP

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -8,9 +8,21 @@ object MarkdownParser {
     // group regex
     private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+$)"
     private const val HEADER_GROUP = "(^#{1,6} .+?$)"
+    private const val QUOTE_GROUP = "(^> .+?$)"
+    private const val ITALIC_GROUP = "" //TODO implement me
+    private const val BOLD_GROUP = "" //TODO implement me
+    private const val STRIKE_GROUP = "" //TODO implement me
+    private const val RULE_GROUP = "" //TODO implement me
+    private const val INLINE_GROUP = "" //TODO implement me
+    private const val LINK_GROUP = "" //TODO implement me
+    private const val BLOCK_CODE_GROUP = "" //TODO implement me
+    private const val ORDER_LIST_GROUP = "" //TODO implement me
 
-    // result regex
-    const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP"
+    //result regex
+    private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP"
+    //+
+    //        "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP"
+    //|$BLOCK_CODE_GROUP|$ORDER_LIST_GROUP optionally
 
     private val elementsPattern by lazy { Pattern.compile(MARKDOWN_GROUPS, Pattern.MULTILINE) }
 
@@ -52,7 +64,7 @@ object MarkdownParser {
             var text: CharSequence
 
             // groups range for iterate by groups
-            val groups = 1..11
+            val groups = 1..3
             var group = -1
             // цикл чтобы итереироваться по группам
             for (gr in groups) {
@@ -90,7 +102,6 @@ object MarkdownParser {
 
                     // text without "{#} "
                     text = string.subSequence(startIndex.plus(level.inc()), endIndex)
-
                     val element = Element.Header(level, text)
                     parents.add(element)
                     lastStartIndex = endIndex
@@ -99,7 +110,11 @@ object MarkdownParser {
                 //QUOTE
                 3 -> {
                     //text without "> "
-                    //TODO implement me
+                    text = string.subSequence(startIndex.plus(2), endIndex)
+                    val subelements = findElements(text)
+                    val element = Element.Quote(text, subelements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
                 }
 
                 //ITALIC
