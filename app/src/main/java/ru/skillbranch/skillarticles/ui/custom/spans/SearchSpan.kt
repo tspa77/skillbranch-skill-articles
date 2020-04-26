ba6862b336ffade6ff88216fc618e85b1218c3fb
Pavel (Home) <tspa77@gmail.com>
2020-04-26 12:54:29 +0700

    parsing - 0:44:34

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/spans/SearchSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/spans/SearchSpan.kt
@@ -1,16 +1,7 @@
 package ru.skillbranch.skillarticles.ui.custom.spans
 
-import android.text.TextPaint
-import android.text.style.BackgroundColorSpan
-import androidx.core.graphics.ColorUtils
+import android.text.style.ForegroundColorSpan
 
-open class SearchSpan(bgColor: Int, private val fgColor: Int) : BackgroundColorSpan(bgColor) {
-    private val alphaColor by lazy {
-        ColorUtils.setAlphaComponent(backgroundColor, 160)
-    }
+open class SearchSpan(fgColor: Int) : ForegroundColorSpan(fgColor) {
 
-    override fun updateDrawState(textPaint: TextPaint) {
-        textPaint.bgColor = alphaColor
-        textPaint.color = fgColor
-    }
 }
